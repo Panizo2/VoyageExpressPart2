8018ae8d66d3bb25fa8c0257edd6fe22f17a6e0b
Panizo2 <88464143+Panizo2@users.noreply.github.com>
2023-06-08 22:36:07 -0300

    DB Finished

---
+++ Compra.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>VoyageExpress | Compra</title>
+  <link rel="stylesheet" href="CSS/bootstrap.css">
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+  <meta name="theme-color" content="#F8F9FA">
+
+  <?php
+    session_start();
+    include 'header.php';
+    $_SESSION['Local'];
+    ?>
+
+<img src="<?php echo "img/Banners/$Local;" ?>" alt="Banner" style="width: 100%; height: auto;">
+
+</head>
+<body>
+
+</body>
+</html>
